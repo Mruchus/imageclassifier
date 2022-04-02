f7be73ea19902b815c5b5160208bdbb2182c2cb8
Hanh Nhi Ngo <44668267+Mruchus@users.noreply.github.com>
2022-04-02 22:27:07 +0100

    Add files via upload

---
+++ RangeLImageClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RangeLImageClassifier
+//
+//  Created by mruchus on 02/04/2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
